15f567ee0603ab7e8f0bd787a2ee93d47b238bef
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-02 23:37:20 +0200

    refactor: subscription

--- trpc/utils/subscription.ts
+++ trpc/utils/subscription.ts
@@ -5,17 +5,9 @@ export async function getSubscriptionStatus(userId: string) {
     where: (s, { eq }) => eq(s.userId, userId),
   })
 
-  if (!subscription) {
-    return {
-      isActive: false,
-      exists: false,
-      status: null,
-    }
-  }
-
   return {
-    isActive: subscription.data.status === 'active' || subscription.data.status === 'trialing',
-    exists: subscription.data.status === 'active' || subscription.data.status === 'trialing' || subscription.data.status === 'past_due' || subscription.data.status === 'unpaid' || subscription.data.status === 'paused' || subscription.data.status === 'incomplete' || subscription.data.status === 'incomplete_expired',
-    status: subscription.data.status,
+    isActive: subscription ? subscription.data.status === 'active' || subscription.data.status === 'trialing' : false,
+    exists: !!subscription,
+    status: subscription ? subscription.data.status : null,
   }
 }
